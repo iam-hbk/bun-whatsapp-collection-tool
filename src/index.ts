265a5a128776c26e5d3ca33730d39caeba6a6c23
iam-hbk <rainheritier@gmail.com>
2023-10-25 13:22:37 +0200

    database set

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
 import express from "express";
+import { readCSV } from "./read_csv";
+import Database from "bun:sqlite";
 const app = express();
 const port = 3000;
 
 const accountSid = Bun.env.TWILIO_ACCOUNT_SID;
 const authToken = Bun.env.TWILIO_AUTH_TOKEN;
 const client = require("twilio")(accountSid, authToken);
+
+let db: Database;
+try {
+  db = await readCSV();
+  db.exec("PRAGMA journal_mode = WAL;");
+  console.log("Database populated successfully");
+} catch (error) {
+  console.error("DATABASE ERROR",(error as Error).message);
+}
 
 app.get("/", async (req, res) => {
   res.send("Whatsapp bot is running!");
@@ -13,18 +24,34 @@ app.get("/", async (req, res) => {
 app.get("/send", async (req, res) => {
   // const { to, message } = req.query;
   // console.log(to, message);
-  const message = await client.messages.create({
-    from: "whatsapp:+14155238886",
-    body: "MR Ronny should buy an iPhone",
-    to: `whatsapp:+27642448112`,
-  });
+  try {
+    const message = await client.messages.create({
+      from: "whatsapp:+14155238886",
+      // from: "whatsapp:+15187543384",
+      body: "Message from Server",
+      to: `whatsapp:+27642448112`,
+    });
 
-  res.send(message);
+    res.json({ message });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 });
 
-app.listen(port, () => {
+app.listen(port, async () => {
   console.log(`Server running on port ${port}`);
 });
 
-// const body = figlet.textSync("P a v e!", "Swan");
-// return new Response(body);
+// Handle process termination
+process.on("SIGINT", closeDatabase);
+process.on("SIGTERM", closeDatabase);
+
+function closeDatabase() {
+  if (db) {
+    db.close();
+    console.log("Database connection closed");
+    process.exit(0);
+  } else {
+    process.exit(0); // Exit the process if there's no database connection
+  }
+}
---
+++ src/read_csv.ts
@@ -0,0 +1,49 @@
+import csvParser from "csv-parser";
+import fs from "fs";
+import { initializeDatabase } from "./bun_base";
+import { Database } from "bun:sqlite";
+
+interface Unlabeled_Data {
+  label: string;
+  text: string;
+}
+// @iam-hbk on Github
+export async function readCSV(): Promise<Database> {
+  const filePath = "src/data/unlabeled_data.csv";
+  const db = await initializeDatabase();
+
+  return new Promise((resolve, reject) => {
+    const data: Unlabeled_Data[] = [];
+    fs.createReadStream(filePath)
+      .pipe(csvParser())
+      .on("data", async (row: Unlabeled_Data) => {
+        data.push(row);
+        const { text } = row;
+        runInsertQuery(db, text);
+      })
+      .on("end", () => {
+        resolve(db);
+      })
+      .on("error", (error) => {
+        reject(error);
+      });
+  });
+}
+
+function runInsertQuery(db: Database, text: string): void {
+  try {
+    const query = "INSERT INTO data (text) VALUES (?);";
+    const prepared_query_statement = db.query(query);
+    prepared_query_statement.run(text);
+  } catch (error) {
+    throw error;
+  }
+
+  // db.run(query, [text], (err) => {
+  //   if (err) {
+  //     reject(err);
+  //   } else {
+  //     resolve();
+  //   }
+  // });
+}
